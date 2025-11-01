45455b95ad71dfeace82eb30d8f7a777447ead61
Akshaypa91 <pagareakshay1294@gmail.com>
2025-11-01 18:30:23 +0530

    heap

---
+++ Heap/maxHeap.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int maxHeap[MAX];
+int sizeMax = 0;
+
+void swap(int *a, int *b) {
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+// Insert into Max Heap
+void insertMax(int value) {
+	if (sizeMax >= MAX) {
+		printf("Heap is full!\n");
+		return;
+	}
+	maxHeap[sizeMax] = value;
+	int i = sizeMax;
+	sizeMax++;
+
+	// Heapify Up
+	while (i != 0 && maxHeap[(i - 1) / 2] < maxHeap[i]) {
+		swap(&maxHeap[(i - 1) / 2], &maxHeap[i]);
+		i = (i - 1) / 2;
+	}
+}
+
+// Delete maximum (root)
+int deleteMax() {
+	if (sizeMax <= 0) return -1;
+
+	int root = maxHeap[0];
+	maxHeap[0] = maxHeap[sizeMax - 1];
+	sizeMax--;
+
+	// Heapify Down
+	int i = 0;
+	while (1) {
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+		int largest = i;
+
+		if (left < sizeMax && maxHeap[left] > maxHeap[largest])
+			largest = left;
+		if (right < sizeMax && maxHeap[right] > maxHeap[largest])
+			largest = right;
+
+		if (largest == i)
+			break;
+
+		swap(&maxHeap[i], &maxHeap[largest]);
+		i = largest;
+	}
+	return root;
+}
+
+void displayMaxHeap() {
+	printf("Max Heap: ");
+	for (int i = 0; i < sizeMax; i++)
+		printf("%d ", maxHeap[i]);
+	printf("\n");
+}
+
+int main() {
+	insertMax(10);
+	insertMax(40);
+	insertMax(30);
+	insertMax(5);
+	displayMaxHeap();
+	printf("Removed Max = %d\n", deleteMax());
+	displayMaxHeap();
+
+	return 0;
+}
