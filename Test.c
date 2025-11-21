83fd30746de74a6d68032b18b5efe99a3ee445f7
Akshaypa91 <pagareakshay1294@gmail.com>
2025-11-22 00:14:39 +0530

    new

--- Test.c
+++ Test.c
@@ -1,42 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Method-1 by Stack
-int* inorderTraversal(struct TreeNode* root, int* returnSize) {
-    int* arr = (int*)malloc(1000 * sizeof(int));
-    *returnSize = 0;
-    stack<struct TreeNode*> st;
-    struct TreeNode* temp = root;
-
-    while (temp != NULL || !st.empty()) {
-        while (temp != NULL) {
-            st.push(temp);
-            temp = temp->left;
-        }
-        temp = st.top();
-        st.pop();
-        arr[(*returnSize)++] = temp->val;
-        temp = temp->right;
-    }
-    return arr;
-}
-
-//Method-2 by Recursion
-int* inorderTraversal(struct TreeNode* root, int* returnSize) {
-    int* arr = (int*)malloc(1000 * sizeof(int));
-    *returnSize = 0;
-    inorderHelper(root, arr, returnSize);
-    return arr;
-}
-
-void inorderHelper(struct TreeNode* root, int* arr, int* returnSize) {
-    if (root == NULL) return;
-    inorderHelper(root->left, arr, returnSize);
-    arr[(*returnSize)++] = root->val;
-    inorderHelper(root->right, arr, returnSize);
-}
-
 int main() {
+    int nums[5] = {1, 1, 4, 1, 1};
+
+    for(int i = 0; i < 5; i++) {
+        if(nums[i] == '4') {
+            goto end;
+        }
+    }
+    end:
+        printf("This is the End \n");
 
     return 0;
 }
